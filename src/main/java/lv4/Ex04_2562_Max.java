810c8fa2f6a8cbbf08edc40b95694e5ca2eeaba0
syh <younghoon211@naver.com>
2024-12-24 12:56:27 +0900

    2562_최댓값

--- src/main/java/lv4/Ex04_2562_Max.java
+++ src/main/java/lv4/Ex04_2562_Max.java
@@ -29,6 +29,32 @@ public class Ex04_2562_Max {
         System.out.println(index);
     }
 
+    //104ms 배열
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int[] arr = new int[9];
+//
+//        int cnt = 1;
+//
+//        for (int i = 0; i < 9; i++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        int max = arr[0];
+//
+//        for (int i = 1; i < 9; i++) {
+//            if (max < arr[i]) {
+//                max = arr[i];
+//                cnt = i + 1;
+//            }
+//        }
+//
+//        System.out.println(max);
+//        System.out.println(cnt);
+//    }
+    
     // 104ms
     // 배열
 //    public static void main(String[] args) throws IOException {
