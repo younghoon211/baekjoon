61d3433e34dd1b53adcc0bb69c6d07777591e5c8
syh <younghoon211@naver.com>
2025-01-01 14:40:22 +0900

    5597

--- src/main/java/lv4/Ex07_5597_WhoDontSubmitAsgn.java
+++ src/main/java/lv4/Ex07_5597_WhoDontSubmitAsgn.java
@@ -1,4 +1,50 @@
 package lv4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ex07_5597_WhoDontSubmitAsgn {
+
+    // boolean
+    // 96ms
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] arr = new boolean[30];
+
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            arr[n - 1] = true;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if (!arr[i]) {
+                System.out.println(i + 1);
+            }
+        }
+
+    }
+
+    // int배열
+    // 100ms
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int[] arr = new int[30];
+//
+//        for(int i = 0; i<28; i++) {
+//            int n = Integer.parseInt(br.readLine());
+//            arr[n-1]++;
+//        }
+//
+//        for(int i = 0; i<30; i++) {
+//            if (arr[i] == 0) {
+//                System.out.println(i+1);
+//            }
+//        }
+//
+//    }
 }
