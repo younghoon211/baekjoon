fa11c96f98dbb15ea926d86e7045f8c237a8eba0
syh <younghoon211@naver.com>
2025-01-05 16:24:22 +0900

    10811

--- src/main/java/lv4/Ex09_10811_BasketFlip.java
+++ src/main/java/lv4/Ex09_10811_BasketFlip.java
@@ -1,4 +1,43 @@
 package lv4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Ex09_10811_BasketFlip {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+
+        int N = Integer.parseInt(arr[0]);
+        int M = Integer.parseInt(arr[1]);
+
+        int[] rslt = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            rslt[i] = i + 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            String[] arr2 = br.readLine().split(" ");
+            int a = Integer.parseInt(arr2[0]) - 1;
+            int b = Integer.parseInt(arr2[1]) - 1;
+
+            while (a < b) {
+                int tmp = rslt[a];
+                rslt[a] = rslt[b];
+                rslt[b] = tmp;
+                a++;
+                b--;
+            }
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(rslt[i] + " ");
+        }
+    }
 }
