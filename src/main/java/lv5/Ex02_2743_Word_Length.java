8250cff6181d9d2bd1a2b33438e2e5fe55e2f1e1
syh <younghoon211@naver.com>
2025-01-08 15:58:51 +0900

    2743

---
+++ src/main/java/lv5/Ex02_2743_Word_Length.java
@@ -0,0 +1,18 @@
+package lv5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex02_2743_Word_Length {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+
+        System.out.println(S.length());
+    }
+}
